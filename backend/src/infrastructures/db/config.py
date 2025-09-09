cea18ecd897a1284f6307507e3cc62eef2c54a6d
Aryan Tamang <104264310+AryanTmg628@users.noreply.github.com>
2025-09-09 22:29:57 +0545

    feat : sqlalchemy db configuration added

---
+++ backend/src/infrastructures/db/config.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
+
+from ..config.settings import get_settings
+
+settings = get_settings()
+
+async_engine = create_async_engine(url=settings.ASYNC_DATABASE_URL, echo=True)
+sync_engine = create_engine(url=settings.SYNC_DATABASE_URL, echo=False)
+
+async_session = async_sessionmaker(
+    bind=async_engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+Base = declarative_base()
