712d7633f5e157c22fda942f88b2bb592ac19ac2
Aryan Tamang <104264310+AryanTmg628@users.noreply.github.com>
2025-09-10 22:30:56 +0545

    feat : made src a python package

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,11 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
+
+from src.applications.authentication.router import auth_router
 
 app = FastAPI(title="Doer Application", description="A simple project for leveling up")
+
+main_router = APIRouter(prefix="/api")
+main_router.include_router(auth_router)
+
+app.include_router(main_router)
