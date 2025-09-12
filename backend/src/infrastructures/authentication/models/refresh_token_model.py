058b9dffe9a746a36a7aa9ff9eeda79c9d3b157e
Aryan Tamang <aryantamangcs@gmail.com>
2025-09-12 09:57:13 +0545

    feat : refresh_token_model added

---
+++ backend/src/infrastructures/authentication/models/refresh_token_model.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.infrastructures.common.models import BaseModel
+from src.infrastructures.common.timestamp_mixin import TimeStampMixin
+
+
+class RefreshTokenModel(
+    BaseModel, TimeStampMixin
+):  # pylint: disable=(too-few-public-methods)
+    """
+    Refresh Token Model to store refresh token details in db
+    """
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("sys_users.id", ondelete="CASCADE"), index=True, nullable=False
+    )
+    refresh_token: Mapped[str] = mapped_column(nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(DateTime(), nullable=False)
