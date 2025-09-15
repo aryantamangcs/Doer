4208ba3ca8fa06f78000e9093f7d06d351b4b1d3
Aryan Tamang <aryantamangcs@gmail.com>
2025-09-15 08:17:57 +0545

    refactor : changed user entity to pure entity logic

--- backend/src/domains/authentication/entities/user_entity.py
+++ backend/src/domains/authentication/entities/user_entity.py
@@ -1,51 +1,79 @@
 from dataclasses import dataclass
-from typing import Optional
-from uuid import UUID
+from uuid import uuid4
+
+from src.shared.exceptions import DomainError
 
 
 @dataclass
-class UserData:
+class User:
     """
-    User Data data class to hold data values
+    User entity with core domain logic
     """
 
     first_name: str
     last_name: str
     username: str
     email: str
-    identifier: UUID
-    password: str
-    id: Optional[int] = None
-
-
-class User:
-    """
-    User entities with attributes of user
-    """
-
-    def __init__(self, user_data: UserData) -> None:
-        """
-        Initializes the attributes
-        """
-        self.id = user_data.id
-        self.first_name = user_data.first_name
-        self.last_name = user_data.last_name
-        self.email = user_data.email
-        self.identifier = user_data.identifier
-        self.password = user_data.password
-        self.username = user_data.username
+    identifier: str  # string of UUID
+    _password: str
 
     def __str__(self) -> str:
         return f"{self.username}_{self.email}"
+
+    def verify_password(self, password: str, hasher) -> bool:
+        """
+        Verifies the password of the user
+        Args:
+            User provided password
+            Hasher service
+        Returns :
+            If password matches returns True else returns False
+        """
+        if hasher.verify(self._password, password):
+            return True
+        return False
 
     def change_password(self, password: str) -> None:
         """
         Changes the password of the user
         """
-        self.password = password
+        if len(password) < 8:
+            raise DomainError(detail="New password must be at least 8 characters")
+        self._password = password
 
     def change_username(self, username: str) -> None:
         """
         Change the username of the user
         """
         self.username = username
+
+    @classmethod
+    def create_user(
+        cls,
+        first_name: str,
+        last_name: str,
+        username: str,
+        email: str,
+        password: str,
+        identifier: str = str(uuid4()),
+    ) -> "User":
+        """
+        Creates the user
+        Args
+            first_name, last_name,username,email,identifier,password
+        Returns
+            instance of User class
+        """
+        if not username or not email:
+            raise ValueError("Username and email is required.")
+
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            _password=password,
+            identifier=identifier,
+        )
+
+        return user
