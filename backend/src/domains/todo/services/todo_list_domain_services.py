e17b6cec1f619cd759a6c568498e2240c7ee0155
Aryan Tamang <aryantamangcs@gmail.com>
2025-09-26 15:43:13 +0545

    refactor : refactor the list member repo

--- backend/src/domains/todo/services/todo_list_domain_services.py
+++ backend/src/domains/todo/services/todo_list_domain_services.py
@@ -1,4 +1,5 @@
 from src.domains.authentication.repositories.user_repository import UserRepo
+from src.domains.todo.services.list_member_domain_services import ListMemberServices
 
 from ..entities.todo_entity import ListMember, TodoList
 from ..repositories.todo_list_repo import TodoListRepository
@@ -9,9 +10,15 @@ class TodoListDomainServices:
     List of methods and services for todo list
     """
 
-    def __init__(self, repo: TodoListRepository, user_repo: UserRepo):
+    def __init__(
+        self,
+        repo: TodoListRepository,
+        user_repo: UserRepo,
+        list_member_service: ListMemberServices,
+    ):
         self.repo = repo
         self.user_repo = user_repo
+        self.list_member_service = list_member_service
 
     async def create_todo_list(self, name: str, owner_id: int) -> TodoList:
         """
@@ -20,7 +27,7 @@ class TodoListDomainServices:
         new_todo_list = TodoList.create(name=name, owner_id=owner_id)
         return await self.repo.add(todo_list=new_todo_list)
 
-    async def add_member(self, todo_list_id: int, user_id: int) -> None:
+    async def add_member(self, todo_list_id: int, user_id: int) -> ListMember:
         """
         Adds member to the new_todo_list
         Args:
@@ -36,8 +43,10 @@ class TodoListDomainServices:
         if not new_member:
             raise ValueError("Member to be added not found")
 
-        ListMember.create(user_id=user_id, todo_list_id=todo_list.id)
-        await self.repo.update(todo_list)
+        new_list_member = await self.list_member_service.add_member(
+            user_id=user_id, todo_list_id=todo_list.id
+        )
+        return new_list_member
 
     async def delete_member(self, todo_list_id: int, user_id: int) -> None:
         """
@@ -55,11 +64,7 @@ class TodoListDomainServices:
         if not new_member:
             raise ValueError("Member to be deleted not found")
 
-        member_exist = [
-            member for member in todo_list.members if member.user_id == user_id
-        ]
-
-        if not member_exist:
-            raise ValueError("User is not a member of the todo list")
-
+        await self.list_member_service.remove_member(
+            user_id=user_id, todo_list_id=todo_list.id
+        )
         await self.repo.update(todo_list)
