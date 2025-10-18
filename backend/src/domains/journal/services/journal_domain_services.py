e444ccab9552f99dd492b90b120abd9d84e8d704
Aryan Tamang <aryantamangcs@gmail.com>
2025-10-18 16:36:17 +0545

    feat : domain services added

--- backend/src/domains/journal/services/journal_domain_services.py
+++ backend/src/domains/journal/services/journal_domain_services.py
@@ -1,5 +1,7 @@
+from src.domains.authentication.repositories.user_repository import UserRepo
 from src.domains.journal.entities.journal_entity import Journal
 from src.domains.journal.repositories.journal_repo import JournalRepository
+from src.shared.exceptions import NotFoundError, ServerError
 
 
 class JournalDomainServices:
@@ -7,8 +9,9 @@ class JournalDomainServices:
     list of methods and services for journal
     """
 
-    def __init__(self, repo: JournalRepository):
+    def __init__(self, repo: JournalRepository, user_repo: UserRepo):
         self.repo = repo
+        self.user_repo = user_repo
 
     async def create_journal(
         self, title: str, description: str, created_by: int
@@ -26,3 +29,19 @@ class JournalDomainServices:
             title=title, description=description, created_by=created_by
         )
         return await self.repo.add(new_journal)
+
+    async def list_journal(self, user_identifier: str) -> list[Journal]:
+        """
+        List all the journal of the user
+        Finds user by the public identifier
+        then finds the jounral associated with the user
+        """
+
+        user = await self.user_repo.get_by_identifier(user_identifier)
+        if not user:
+            raise NotFoundError(detail="User not found")
+        if not user.id:
+            raise ServerError(detail="User id is set none")
+
+        all_journals = await self.repo.filter(where={"created_by": user.id})
+        return all_journals
